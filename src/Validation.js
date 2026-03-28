a12007a601756fe91f42f36712502475184cda36
Arnav Thorat <arnavthorat78@gmail.com>
2026-03-28 19:38:33 +1100

    Add support for new Minecraft versioning system

--- src/Validation.js
+++ src/Validation.js
@@ -3,7 +3,21 @@ class Validation {
     const releaseTest = /^1\.(0|[1-9]\d*)(?:\.(0|[1-9]\d*))?(?:-(pre|rc)([1-9]\d*))?$/; // also includes pre-releases and release candidates
     const snapshotTest = /^\d{2}w(0[1-9]|[1-4]\d|5[0-3])[a-z]$/;
 
-    if ((!releaseTest.test(version) && !snapshotTest.test(version)) || !version) return false;
+    const newReleaseTest = /^\d{2}\.\d+$/;
+    const newSnapshotTest = /^\d{2}\.\d+-snapshot-[1-9]\d*$/;
+    const newPreReleaseTest = /^\d{2}\.\d+-pre-[1-9]\d*$/;
+    const newRcTest = /^\d{2}\.\d+-rc-[1-9]\d*$/;
+
+    if (
+      (!releaseTest.test(version) &&
+        !snapshotTest.test(version) &&
+        !newReleaseTest.test(version) &&
+        !newSnapshotTest.test(version) &&
+        !newPreReleaseTest.test(version) &&
+        !newRcTest.test(version)) ||
+      !version
+    )
+      return false;
     return true; // TODO should it also return which was true? snapshot or release
   }
 
